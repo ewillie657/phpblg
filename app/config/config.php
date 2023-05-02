9c5617062ae5983ee90293b6e98305114dcb9501
Edward Willie <williee541@gmail.com>
2023-05-02 15:05:52 -0500

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+
+    //App Root
+    define ('APPROOT',dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost:81/phpmvc');
+ 
+    // Site name
+    define('SITENAME', 'SharePost');
+
+    // App Version
+    define('APPVERSION', '1.0.0');
+
+?>
---
+++ app/controllers/Pages.php
@@ -0,0 +1,39 @@
+<?php
+
+    class Pages extends Controller {
+
+        public function __construct(){
+
+        }
+
+        public function index(){
+
+            if(isLoggedIn()){
+                redirect('posts');
+            }
+
+            $data = [
+                'title' => 'SharePosts',
+                'description' => 'Simple social network built on the Edward Willie PHP framework'
+        ];
+
+            $this->view('pages/index', $data);
+
+        }
+
+        public function about(){
+
+            $data = [
+                'title' => 'About',
+                'description' => 'App tp share posts with other users'
+            ];
+
+            $this->view('pages/about', $data);
+        }
+
+        
+
+    }
+
+
+?>
